9fc954eb7cd2602279393804e69bd621a0f088b7
Puneet Bhardwaj <66798951+Puneet489@users.noreply.github.com>
2022-10-11 11:11:17 +0530

    Create packagespart2.java

---
+++ packagespart2.java
@@ -0,0 +1,11 @@
+package h;
+public class policy{
+    public void show(){
+        System.out.println("Inside policy class");
+    }
+    
+    public static void main(String args[]){
+        policy p = new policy();
+        p.show();
+    }
+}
